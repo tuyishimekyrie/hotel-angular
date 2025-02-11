3f8252a175deb10c31cc34da38ffcf2e94b4a203
tuyishimekyrie <tuyishimehope01@gmail.com>
2025-02-11 09:56:35 +0200

    Refactor codes

---
+++ src/app/directives/highlight-completed-todos.directive.spec.ts
@@ -0,0 +1,8 @@
+import { HighlightCompletedTodosDirective } from './highlight-completed-todos.directive';
+
+describe('HighlightCompletedTodosDirective', () => {
+  it('should create an instance', () => {
+    const directive = new HighlightCompletedTodosDirective();
+    expect(directive).toBeTruthy();
+  });
+});
